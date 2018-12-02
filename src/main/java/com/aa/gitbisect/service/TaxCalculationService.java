ffb5cd8b49ea19d96fb5a8e69ad1d3bff0b27506
AlexeyAnufriev <ice.sumy@gmail.com>
2018-12-02 01:53:29 +0100

    Tax service definition

---
+++ src/main/java/com/aa/gitbisect/service/TaxCalculationService.java
@@ -0,0 +1,10 @@
+package com.aa.gitbisect.service;
+
+import java.math.BigDecimal;
+
+public class TaxCalculationService {
+
+    public BigDecimal getNetSalaryInCurrency(BigDecimal grossSalaryInCurrency, int taxInPercent) {
+        return null;
+    }
+}
